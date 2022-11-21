2cc8a85950ddb7bb37c529c040e26139746810de
Stefan Karasmilov <stefan.karasmilov@540deg.com>
2022-11-21 08:45:28 +0100

    adjusts

--- src/core/infrastructure/Comic/apiComicRepository.ts
+++ src/core/infrastructure/Comic/apiComicRepository.ts
@@ -1,10 +1,9 @@
 import { ComicRepository } from '../../domain/model/Comic/ComicRepository'
-import { api } from '../api'
 import { Comic } from '../../domain/model/Comic/Comic'
 
 export const apiComicRepository: ComicRepository = {
   findBy: async (characterId: string) => {
-  const response = await fetch(`/comics-${characterId}.json`)
-  return await response.json() as Comic[]
-},
+    const response = await fetch(`/comics-${characterId}.json`)
+    return await response.json() as Comic[]
+  }
 }
